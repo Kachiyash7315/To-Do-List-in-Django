dcd239c872174f0a7a912a1444dab2ccd27f1a6c
Your Name <you@example.com>
2022-06-01 13:25:09 +0530

    First Commit

---
+++ Todoproject/todoapp/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import Task
+
+
+class TaskForm(forms.ModelForm):
+    content = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Add task.....'}))
+
+    class Meta:
+        model = Task
+        fields = ['content']
+
+
+class UpdateTaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = '__all__'
